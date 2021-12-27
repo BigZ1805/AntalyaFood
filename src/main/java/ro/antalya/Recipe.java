3676f10a58d917ddd9b659037bee24880c234b13
Mihai <vigu.mihai@gmail.com>
2021-12-27 19:54:58 +0200

    Added final confirmation feature

--- src/main/java/ro/antalya/Recipe.java
+++ src/main/java/ro/antalya/Recipe.java
@@ -18,14 +18,11 @@ public class Recipe {
             if (Iute) {
                 values.remove("KETCHUP_DULCE");
                 System.out.println("Deci cu de toate si iute! Am inteles!");
-            }
-            else {
+            } else {
                 values.remove("SOS_IUTE");
                 System.out.println("Deci cu de toate si dulce! Am inteles!");
             }
-        }
-        else
-        {
+        } else {
             System.out.println("OK! Deci o facem custom! Hai sa vedem! Sa fie iute? DA/NU");
             boolean Iute = scanner.nextBoolean();
             if (Iute) {
@@ -34,8 +31,7 @@ public class Recipe {
                 String Scoatem = scanner.next();
                 values.remove(Scoatem);
                 System.out.println("OK, fara " + Scoatem);
-            }
-            else {
+            } else {
                 values.remove("SOS_IUTE");
                 System.out.println("OK, deci dulce! In rest ce mai scoatem?");
                 String Scoatem = scanner.next();
@@ -52,64 +48,75 @@ public class Recipe {
                 System.out.println("Mai scoatem ceva? DA/NU");
                 MaiScoatem = scanner.nextBoolean();
             }
-
+            finalConfirmation();
         }
     }
 
-
-    public void applyRecipe(Product product) {
+         public void finalConfirmation() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Lista de ingrediente este urmatoarea: " + values + " Ramane asa? DA/NU");
+            boolean Confirmation = scanner.nextBoolean();
+            if (Confirmation) {
+                System.out.println("Comanda confirmata! Va multumim!");
+            } else {
+                System.out.println("Mai scoatem ceva? DA/NU");
+                boolean MaiScoatem2 = scanner.nextBoolean();
+                while (MaiScoatem2) {
+                    System.out.println("Ce mai scoatem?");
+                    String Scoatem = scanner.next();
+                    values.remove(Scoatem);
+                    System.out.println("OK, am scos si " + Scoatem);
+                    System.out.println("Mai scoatem ceva? DA/NU");
+                    MaiScoatem2 = scanner.nextBoolean();
+                }
+                System.out.println("Comanda confirmata! Va multumim!");
+            }
+        }
+    public void applyRecipe(Product product){
         switch (product) {
-        case SHAORMA_PUI_MICA:
-            getRecipe(Product.SHAORMA_PUI_MICA);
-            values.remove("CARNE_VITA");
-            values.remove("CARNE_PORC");
-            values.remove("CHIFLE");
-            System.out.println("OK! Atunci ramane sa punem " + values);
-        break;
-        case SHAORMA_PUI_MARE:
-            getRecipe(Product.SHAORMA_PUI_MARE);
-            values.remove("CARNE_VITA");
-            values.remove("CARNE_PORC");
-            values.remove("CHIFLE");
-            System.out.println("OK! Atunci ramane sa punem " + values);
-            break;
-        case SHAORMA_VITA_MICA:
-            getRecipe(Product.SHAORMA_VITA_MICA);
-            values.remove("CARNE_PUI");
-            values.remove("CARNE_PORC");
-            values.remove("CHIFLE");
-            System.out.println("OK! Atunci ramane sa punem " + values);
-            break;
-        case SHAORMA_VITA_MARE:
-            getRecipe(Product.SHAORMA_VITA_MARE);
-            values.remove("CARNE_PUI");
-            values.remove("CARNE_PORC");
-            values.remove("CHIFLE");
-            System.out.println("OK! Atunci ramane sa punem " + values);
-            break;
-        case SHAORMA_MIX:
-            getRecipe(Product.SHAORMA_MIX);
-            values.remove("CHIFLE");
-            System.out.println("OK! Atunci ramane sa punem " + values);
-            break;
-        case KEBAB_PUI:
-            getRecipe(Product.KEBAB_PUI);
-            values.remove("CARNE_VITA");
-            values.remove("CARNE_PORC");
-            values.remove("LIPIE");
-            System.out.println("OK! Atunci ramane sa punem " + values);
-
-            break;
-        case KEBAB_VITA:
-            getRecipe(Product.KEBAB_VITA);
-            values.remove("CARNE_PUI");
-            values.remove("CARNE_PORC");
-            values.remove("LIPIE");
-            System.out.println("OK! Atunci ramane sa punem " + values);
-            break;
+            case SHAORMA_PUI_MICA:
+                getRecipe(Product.SHAORMA_PUI_MICA);
+                values.remove("CARNE_VITA");
+                values.remove("CARNE_PORC");
+                values.remove("CHIFLE");
+                break;
+            case SHAORMA_PUI_MARE:
+                getRecipe(Product.SHAORMA_PUI_MARE);
+                values.remove("CARNE_VITA");
+                values.remove("CARNE_PORC");
+                values.remove("CHIFLE");
+                break;
+            case SHAORMA_VITA_MICA:
+                getRecipe(Product.SHAORMA_VITA_MICA);
+                values.remove("CARNE_PUI");
+                values.remove("CARNE_PORC");
+                values.remove("CHIFLE");
+                break;
+            case SHAORMA_VITA_MARE:
+                getRecipe(Product.SHAORMA_VITA_MARE);
+                values.remove("CARNE_PUI");
+                values.remove("CARNE_PORC");
+                values.remove("CHIFLE");
+                break;
+            case SHAORMA_MIX:
+                getRecipe(Product.SHAORMA_MIX);
+                values.remove("CHIFLE");
+                break;
+            case KEBAB_PUI:
+                getRecipe(Product.KEBAB_PUI);
+                values.remove("CARNE_VITA");
+                values.remove("CARNE_PORC");
+                values.remove("LIPIE");
+                break;
+            case KEBAB_VITA:
+                getRecipe(Product.KEBAB_VITA);
+                values.remove("CARNE_PUI");
+                values.remove("CARNE_PORC");
+                values.remove("LIPIE");
+                break;
+        }
     }
-    }
-
 }
+
 
 
